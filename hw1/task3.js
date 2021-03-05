a8236c6902a608fda32d7d91afa74eed0dacefe8
dekunma <William@dekun.me>
2021-03-05 13:40:20 +0800

    corrected spelling mistake

--- hw1/task3.js
+++ hw1/task3.js
@@ -1,7 +1,7 @@
 /**
  * Task 3:
  * -------
- * Write a function that returns the current data,
+ * Write a function that returns the current date, month, and year
  * in the following format:
  * dd/mm/yyyy
  * 
@@ -10,6 +10,6 @@
  * Google 'String template literals' and use it instead.
  * 
  * Hint:
- * Google how to use 'Date' to get the current time, date, and year
+ * Google how to use 'Date' to get the current date, month, and year
  * before moving forward
  */
